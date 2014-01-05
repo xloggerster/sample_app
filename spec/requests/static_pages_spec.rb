0af803c52ce466b98fd790542c5349de0c0ac21a
xlogger <xloggerster@gmail.com>
2014-01-05 23:37:37 +0800

    Auto test with Guard, Spork, RubyTest Sublime Plugin

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
 
 	describe "Help page" do
@@ -14,6 +19,11 @@ describe "Static pages" do
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
+		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
 	end
 
@@ -28,5 +38,17 @@ describe "Static pages" do
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
+
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end		
 
 end
