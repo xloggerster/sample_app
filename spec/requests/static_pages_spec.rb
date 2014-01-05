acb650687de3e5503fb84ee04a48347ec74c5b82
xlogger <xloggerster@gmail.com>
2014-01-05 21:41:44 +0800

    Embedded Ruby

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,5 +22,11 @@ describe "Static pages" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
 		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
 	end
+
 end
